b87c438115c31f58dd3e82f943697e627a9a3635
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-02-08 08:31:47 -0600

    intro to tdd

--- spec/support/server.spec.js
+++ spec/support/server.spec.js
@@ -14,4 +14,27 @@ describe('get messages', () => {
       done();
     });
   });
+
+  it('should return return a list, thats not empty', (done) => {
+    request.get('http://localhost:3000/messages', (err, res) => {
+      expect(JSON.parse(res.body).length).toBeGreaterThan(0);
+      done();
+    });
+  });
+});
+
+describe('get messages from user', () => {
+  it('should return 200 ok', (done) => {
+    request.get('http://localhost:3000/messages/tim', (err, res) => {
+      expect(res.statusCode).toEqual(200)
+      done();
+    });
+  });
+
+  it('name should be tim', (done) => {
+    request.get('http://localhost:3000/messages/tim', (err, res) => {
+      expect(JSON.parse(res.body)[0].name).toEqual('tim')
+      done();
+    });
+  });
 });
